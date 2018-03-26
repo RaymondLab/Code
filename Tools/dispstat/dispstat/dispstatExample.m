800675d3f5e1dc59e304a9098dc365ee82f134c0
Max Gagnon <max.gagnon11@gmail.com>
2018-03-26 15:26:03 -0700

    Add code that reads Igor file and adds code that helps display progress percentage of script

---
+++ Tools/dispstat/dispstat/dispstatExample.m
@@ -0,0 +1,27 @@
+clc;  
+fprintf('Step 1: ')
+dispstat('','init')
+length = 10000;
+
+ for i = 1:length
+     dispstat(sprintf('%i%%', floor(i / length * 100)), 'timestamp')
+ end
+
+dispstat('DONE', 'timestamp',z 'keepthis')
+ 
+ 
+fprintf('Step 2: ')
+
+ for i = 1:length
+     dispstat(sprintf('%i%%', floor(i / length * 100)), 'timestamp')
+ end
+ 
+dispstat('DONE', 'timestamp', 'keepthis')
+ 
+fprintf('Step 3: ')
+
+ for i = 1:length
+     dispstat(sprintf('%i%%', floor(i / length * 100)), 'timestamp')
+ end
+ 
+ dispstat('DONE', 'timestamp', 'keepthis')
