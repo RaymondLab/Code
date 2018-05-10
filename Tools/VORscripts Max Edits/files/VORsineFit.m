4f8007be72c27db5beb7879a5590d07e78c4bd95
Max Gagnon <max.gagnon11@gmail.com>
2018-05-10 11:55:53 -0700

    Alternative Version of VORSineFirt

--- Tools/VORscripts Max Edits/files/VORsineFit.m
+++ Tools/VORscripts Max Edits/files/VORsineFit.m
@@ -18,51 +18,51 @@
 % Modified 1/20/14 to remove vertical eye calculations
 
 
-function R = VORsineFit(data, tstart, tstop, sinefreq, labels, timepts,velthres,oldSaccade, ploton)
-% close all
-clf
-set(0, 'DefaultFigurePaperPositionMode', 'auto');
-[~, filename] = fileparts(pwd);
+function R = VORsineFit(data, tstart, tstop, sinefreq, labels, timepts, velthres, ploton, saccadeWindow)
+%% Create R data Array and other parameters
 
-%% Get sampling rate
-samplerate = data(1).samplerate;
-nsegs = length(tstart); % Number of segments to analyze
-
-%% Set up cell structure to hold all R.data
+% Set up cell structure to hold all R.data
 header = {'timept'  'start' 'end'   'eyeHgain' 'eyeHphase'...
     'headamp' 'headangle' 'drumamp' 'drumangle' 'eyeHamp' 'eyeHangle'...
     'saccadeFrac' 'rsquare' 'pval' 'variance','nGoodCycles'};
+
+nSegs = length(tstart);
 nCols = length(header);
-R.data = NaN(nsegs,nCols); % Main array for storing R.data
+
+R.data = NaN(nSegs,nCols);
 R.header = header;
 R.labels = labels;
+
 if ~exist('timepts','var')
     timepts = mean([tstart tstop],2);
 end
 R.data(:,strcmpi(header,'timept')) = timepts;
 
-%% Default parameters
+% Other parameters
 if ~exist('velthres','var')
-    velthres = 100;
-    oldSaccade = 0;
+    velthres = .7;
 end
 
 if ~exist('ploton','var')
     ploton = 1;
 end
 
-presaccade = 0.1;   % time to exclude before saccade (s)
-postsaccade = 0.3;  % time to exclude after saccade (s)
+[~, filename] = fileparts(pwd);
+samplerate = data(1).samplerate;
+% time to exclude before saccade (s)
+presaccade = saccadeWindow(1);   
+% time to exclude after saccade (s)
+postsaccade = saccadeWindow(2);
+% convert back to # of datapoints
+presaccadeN = round(presaccade*samplerate);
+postsaccadeN = round(postsaccade*samplerate);
 
 fprintf('Threshold = %g\n',velthres)
 fprintf('Presaccade = %g ms\n',presaccade)
 fprintf('Postsaccade = %g ms\n',postsaccade)
 
-presaccadeN = round(presaccade*samplerate); %convert back to # of datapoints
-postsaccadeN = round(postsaccade*samplerate); %convert back to # of datapoints
-
-%% Cycle through each segment
-for count = 1:nsegs
+%% === Cycle through each segment ====================================== %%
+for count = 1:nSegs
 
     % Skip segments where frequency or start time is NaN
     if isnan(sinefreq(count)) || isnan(tstart(count))
@@ -112,34 +112,143 @@ for count = 1:nsegs
 
     %% === DESACCADE =================================================== %%
     % First pass remove baseline eye mvmt
-    if ~oldSaccade
-        keep = abs(eyevelH) < 1.5*velthres;        % First pass remove saccades
-        b = regress(eyevelH(keep), vars(keep,:));  % Initial fit
-        eyevelHtemp = eyevelH - vars*b;            % Subtract fitted sine
-    else
-        eyevelHtemp = eyevelH;                      % OLD METHOD
-    end
+    %keep = abs(eyevelH) < 1.5*velthres;        % First pass remove saccades
+    % b = regress(eyevelH(keep), vars(keep,:));  % Initial fit
+    
+    % fft
+    figure(666666)
+    plot(eyevelH)
+    
+    Y = fft(eyevelH);
+    L = length(Y);
+    Fs = 1000;
+    P2 = abs(Y/L);
+    P1 = P2(1:L/2+1);
+    P1(2:end-1) = 2*P1(2:end-1);
+    f = Fs*(0:(L/2))/L;
+    figure(6666667)
+    plot(f(1:2000),P1(1:2000) )
+    title('Single-Sided Amplitude Spectrum of X(t)')
+    xlabel('f (Hz)')
+    ylabel('|P1(f)|')
+    
+    
+    
+    
+    fit1 = vars *b;
+    eyevelHtemp = eyevelH - vars*b;            % Subtract fitted sine
 
     % Find saccades in eye movement and blank out an interval on either side
-    [eyevelH_des_temp, omitCenters] = desaccadeVel(eyevelHtemp, velthres, presaccadeN, postsaccadeN);
+    [eyevelH_des_temp, omitCenters, rawThres1] = desaccadeVelNew(eyevelHtemp, presaccadeN, postsaccadeN, velthres);
+    %[eyevelH_des_temp, omitCenters] = desaccadeVel(eyevelHtemp, velthres,  presaccadeN, postsaccadeN);
     omitH = isnan(eyevelH_des_temp);
-    eyevelH_des = eyevelH;
-    eyevelH_des(omitH) = NaN;
-    % Fraction saccades
+    eyevelH_des1 = eyevelH;
+    eyevelH_des1(omitH) = NaN;
+    
+    
+    % judge fit
+%     b = regress(eyevelH_des1(~omitH), vars(~omitH));
+%     tempFit = vars(~omitH)*b;
+    % subtracted fitted sine
+%     eyevelHtemp = eyevelH_des1(~omitH) - tempFit;
+    % calc MSE
+%     mseList(1) = mean(eyevelHtemp.^2)
+    
+%     fitThres = 750; % CHANGE THIS!!!!!
+%     doExtra = false;
+    
+    
+%     figure(8888); clf
+%     title('First pass')
+%     hold on
+%     plot(eyevelH)
+%     plot(eyevelH_des1)
+    
+    
+%     velthresTemp = velthres;
+%     jj = 1;
+%     if mseList(end) > fitThres
+%         while mseList(end) > fitThres || mseList(end-1) == mseList(end)
+%             
+%             jj = jj + 1;
+%             % try a tighter threshold
+%             velthresTemp = velthresTemp - (velthres*.05);
+%             b = regress(eyevelH, vars);
+%             eyevelHtemp = eyevelH - vars*b;
+%             % Find saccades in eye movement and blank out an interval on either side
+%             [eyevelH_des_temp, omitCenters, rawThres] = desaccadeVelNew(eyevelHtemp, presaccadeN, postsaccadeN, velthresTemp);
+%             omitH = isnan(eyevelH_des_temp);
+%             eyevelH_des = eyevelH;
+%             eyevelH_des(omitH) = NaN;
+% 
+%             % judge fit
+%             b = regress(eyevelH_des(~omitH), vars(~omitH, :));
+%             tempFit = vars*b;
+% 
+%             % subtracted fitted sine
+%             eyevelHtemp = eyevelH_des - tempFit;
+%             % calc MSE
+%             mseList(jj) = nanmean(eyevelHtemp.^2)
+% 
+% 
+%             % PLOT 
+%             figure(8889)
+%             clf
+%             title('Next pass')
+%             hold on
+%             plot(eyevelH, 'k')
+%             plot(eyevelH_des1, 'b')
+%             plot(eyevelH_des, 'r')
+%             plot(tempFit, 'g')
+%             plot(tempFit + rawThres(1), 'g');
+%             plot(tempFit + rawThres(2), 'g');
+%             plot(tempFit + rawThres1(1), ':b');
+%             plot(tempFit + rawThres1(2), ':b');
+%             set(gcf,'Position',[10 260 1300 500]);
+%             ylim([-500, 500])
+%             if abs(mseList(end) - mseList(end-1)) < 30
+%                 break
+%             end
+% 
+% 
+%         end
+%     end
+%     clear mseList
+    
+
+    
+    
     R.data(count,strcmp(header,'saccadeFrac')) = mean(omitH);
 
     %% === Plot the pre and post-saccade removal velocity traces ======= %%
     % Plot eye velocity trace with/without saccades
+    % Smooth to affect appearance - doesn't change actual traces
     if ploton
         figure(count); clf
-
-        % Smooth to affect appearance - doesn't change actual traces
-        plot_eyevelH = smooth(eyevelH,50);
+        plot_eyevelH = smoothdata(eyevelH, 'movmean', 10);
         plot_eyevelH_des = plot_eyevelH;
         plot_eyevelH_des(omitH) = NaN;
-        plot(time,plot_eyevelH,'k',time, plot_eyevelH_des,'b');
-        xlim([0 40]);     ylim([-30 30])
-        xlabel('Time (s)');    set(gcf,'Position',[10 100 1000 300]);
+        %plot(time,plot_eyevelH,'k',time, plot_eyevelH_des,'b');
+        plot(time,eyevelH, 'k',time, eyevelH_des1, 'b')
+        hold on
+        if exist('eyevelH_des', 'var')
+            plot(time, eyevelH_des, 'b')
+        end
+        
+        xlim([0 length(plot_eyevelH)/1000]);     ylim([-200 200])
+        xlabel('Time (s)');    set(gcf,'Position',[10 50 1500 300]);
+        yy = linspace(0, length(plot_eyevelH)/1000, length(eyevelHtemp));
+        hold on
+
+        if exist('rawThres', 'var')
+            plot(yy, tempFit + rawThres(1), ':r');
+            plot(yy, tempFit + rawThres(2), ':r');
+        else
+            plot(yy, fit1 + rawThres1(1), ':r');
+            plot(yy, fit1 + rawThres1(2), ':r');
+        end
+
+        
     end
 
 
@@ -203,7 +312,12 @@ for count = 1:nsegs
         figure(count); hold on
         y = vars*b;
         plot(time, y,'r','LineWidth',1)
-        title(sprintf('%s %s %.1f Amp: %.2f Phase: %.2f',filename,datatype, timepts(count), eyevelH_amp, eyevelH_rel_phase),'interpreter','none')
+        section = {'Test 1a', 'Test 1a', 'Test 1a', 'Test 1a', 'Test 1a', 'Test 1a', ...
+                   'Test 1b', 'Test 1b', 'Test 1b', 'Test 1b', 'Test 1b', 'Test 1b', ...
+                   'Train 1', 'Test 2a', 'Test 2b', 'Train 2', ...
+                   'Test 2a', 'Test 2a', 'Test 2a', 'Test 2a', 'Test 2a', 'Test 2a', ...
+                   'Test 2b', 'Test 2b', 'Test 2b', 'Test 2b', 'Test 2b', 'Test 2b'};
+        title(sprintf('%s   %s  %s  Amp: %.2f  Phase: %.2f',filename,datatype, section{count}, eyevelH_amp, eyevelH_rel_phase),'interpreter','none')
     end
 
     %% === Get Cycle by Cycle Average ================================== %%
@@ -221,10 +335,10 @@ for count = 1:nsegs
 
     for i = 1:nCycles
         eyevelAll(i,:) = eyevelH(zeroCross(i):zeroCross(i)+cycleLength-1)';
-        eyevelDesAll(i,:) = eyevelH_des(zeroCross(i):zeroCross(i)+cycleLength-1)';
+        eyevelDesAll(i,:) = eyevelH_des1(zeroCross(i):zeroCross(i)+cycleLength-1)';
         headAll(i,:) = headvel(zeroCross(i):zeroCross(i)+cycleLength-1)';
         if i<nCycles
-            eyevel2cycle(i,:) = eyevelH_des(zeroCross(i):zeroCross(i)+cycleLength*2-1)'; %*** temp
+            eyevel2cycle(i,:) = eyevelH_des1(zeroCross(i):zeroCross(i)+cycleLength*2-1)'; %*** temp
         end
     end
 
@@ -245,186 +359,34 @@ for count = 1:nsegs
         end
     end
 
-    % Max G 11/17 - Don't take the mean of a vector. This happens when ther
-    % e is only one good cycle. When taking a mean of a vector, it returns
-    % a scalar. That's bad.
-    [row, ~] = size(eyevelAll(~badCycles,:));
-    if row > 1
-        eyevelCycleMean = nanmean(eyevelAll(~badCycles,:));
-    elseif row == 1
-        eyevelCycleMean = eyevelAll(~badCycles,:);
-    elseif row == 0
+    goodCount =  sum(~badCycles);
+    if goodCount > 0
+        eyevelCycleMean = nanmean(eyevelAll(~badCycles,:), 1);
+    else
         eyevelCycleMean = zeros(size(eyevelAll(1,:)));
     end
 
-    %eyevelCycleMean = nanmean(eyevelAll(~badCycles,:));
-    R.eyevelCycleMean{count}  = eyevelCycleMean;
-    goodCount =  sum(~badCycles);
+    R.eyevelCycleMean{count}  = eyevelCycleMean;   
     R.data(count,strcmp(header,'nGoodCycles')) = goodCount;
 
     %% === Plot Cycle by Cycle Average ================================= %%
     if ploton     
         figure(count+100); clf;
-
+        % plot data
         ttCycle = (1:cycleLength)/samplerate;
         plot(ttCycle, smooth(eyevelCycleMean, 50),'b'); hold on
         plot(ttCycle, smooth(eyevelDesMean, 50), 'g');
         plot(ttCycle, sin(2*pi*freq*ttCycle + deg2rad(eyevelH_rel_phase+180))*eyevelH_amp,'r')
         plot(ttCycle, headMean, 'k',ttCycle,zeros(size(ttCycle)),'--k');
-
+        % Cosmetics
         box off
-        ylim([-15 15]);   xlim([0 max(ttCycle)])
+        set(gcf,'Position',[10 430 600 350]);
+        ylim([-30 30]);   xlim([0 max(ttCycle)])
         ylabel('deg/s');  xlabel('Time (s)');
-        %     plot(t, mean(Stim_avg), 'k');
-
-        title(['Hor. Eye Vel: ', datatype ' ' num2str(timepts(count))]);
-        text (.1, 13.5, ['Good cycles: ', num2str(goodCount), '/', num2str(length(zeroCross))],'FontSize',10);
-        text (.1, 12, ['Eye amp: ', num2str(eyevelH_amp,3)],'FontSize',10);
+        title(['Hor. Eye Vel: ', datatype ' ', section{count}]);
+        text (.01, 27, ['Good cycles: ', num2str(goodCount), '/', num2str(length(zeroCross))],'FontSize',10);
+        text (.01, 23, ['Eye amp: ', num2str(eyevelH_amp,3)],'FontSize',10);
         legend({'Desaccaded full cycle removed', 'Desaccaded segments','Sine fit','Stimulus'},'EdgeColor','w')
         drawnow;
     end
-
-
-
-    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-    % Added by Max Gagnon 11/17. This section takes the difference of the
-    % last three (t = 30) recording's means and the first three (t = 0) recording's
-    % mean and plots them.
-    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-    if count == 1 % initialize each vector
-
-        goodCountSumBegin       = [];
-        goodCountSumEnd         = [];
-        trueSegments            = length(tstart(~isnan(tstart)));
-        headVelSegments         = [];
-        drumVelSegments         = [];
-        startEyeVelCycleSegments = [];
-        startEyeVelDesSegments  = [];
-        endEyeVelCycleSegments  = [];
-        endEyeVelDesSegments    = [];
-    end
-
-    % keep a running sum of each waveform
-    if count == 1|| count == 2 || count == 3
-
-        % get head vel. segments
-        headVelLong     = datchandata(dataseg,'hhvel');
-        headVelSegments = [headVelSegments; headVelLong(zeroCross(i):zeroCross(i)+cycleLength-1)'];
-
-        % get drum vel. segments
-        drumVelLong     = datchandata(dataseg,'htvel');
-        drumVelSegments = [drumVelSegments; drumVelLong(zeroCross(i):zeroCross(i)+cycleLength-1)'];
-
-        % record the number of good cycles for each segment
-        goodCountSumBegin         = [goodCountSumBegin; goodCount];
-        startEyeVelCycleSegments  = [startEyeVelCycleSegments; (smooth(eyevelCycleMean, 50) .* goodCount)'];
-        startEyeVelDesSegments    = [startEyeVelDesSegments; (smooth(eyevelDesMean, 50) .* goodCount)'];
-
-    elseif count == (trueSegments - 3) || count == (trueSegments - 2) || count == (trueSegments - 1) % fourth to last, third to last, second to last
-
-        % get head vel. segments
-        headVelLong     = datchandata(dataseg,'hhvel');
-        headVelSegments = [headVelSegments; headVelLong(zeroCross(i):zeroCross(i)+cycleLength-1)'];
-
-        % get drum vel. segments
-        drumVelLong     = datchandata(dataseg,'htvel');
-        drumVelSegments = [drumVelSegments; drumVelLong(zeroCross(i):zeroCross(i)+cycleLength-1)'];
-
-        goodCountSumEnd     = [goodCountSumEnd; goodCount];
-        endEyeVelCycleSegments  = [endEyeVelCycleSegments; (smooth(eyevelCycleMean, 50) .* goodCount)'];
-        endEyeVelDesSegments    = [endEyeVelDesSegments; (smooth(eyevelDesMean, 50) .* goodCount)'];
-
-    end
-end
-
-% get the mean of the head vel over the 6 trials in question
-headVelMean = mean(headVelSegments, 1);
-
-% get the mean of the drum vel over the 6 trials in question
-drumVelMean = mean(drumVelSegments, 1);
-
-% calc mean of each channel
-startEyeVelCycleMean = sum(startEyeVelCycleSegments,1) ./ sum(goodCountSumBegin);
-startEyeVelDesMean  = sum(startEyeVelDesSegments, 1) ./ sum(goodCountSumBegin);
-endEyeVelCycleMean  = sum(endEyeVelCycleSegments, 1) ./ sum(goodCountSumEnd);
-endEyeVelDesMean    = sum(endEyeVelDesSegments, 1) ./ sum(goodCountSumEnd);
-
-% get difference of the means
-BlueDiff = endEyeVelCycleMean - startEyeVelCycleMean;
-GreenDiff = endEyeVelDesMean - startEyeVelDesMean;
-
-% data of plot
-figure(count+1000); clf;
-ttCycle = (1:cycleLength)/samplerate;
-plot(ttCycle, BlueDiff,'b'); hold on
-plot(ttCycle, GreenDiff, 'g');
-diffMeanData = data(1);
-diffMeanData.data = BlueDiff;
-%diffMeanData.chanlabel = 'htvel';
-finalLabel = 'VOR 1Hz';
-
-%% This is a modified version of the  VORsineFit function that
-% specifically handles the mean waveforms. In order to accomadate
-% this new functionality, there would have to be to many changes to
-% the original function, so a new one was created. See Max Gagnon
-% for more details.
-[eyevelH_offset, eyevelH_rel_phase, eyevelH_amp, eyeHgain] = VORsineFitMaxMod( sinefreq(1), data(1).samplerate, BlueDiff', headVelMean', drumVelMean');
-plot(ttCycle, eyevelH_offset+sin(2*pi*freq*ttCycle + deg2rad(eyevelH_rel_phase+180))*eyevelH_amp,'r')
-plot(ttCycle, headMean, 'k',ttCycle,zeros(size(ttCycle)),'--k');
-box off
-
-% Cosmetics of plot
-ylim([-15 15]);   xlim([0 max(ttCycle)])
-ylabel('deg/s');  xlabel('Time (s)');
-title(['Delta Hor. Eye Vel: ', datatype(1:8) ' ']);
-%text (.1, 13.5, ['Good cycles: ', num2str(goodCount), '/', num2str(nCycles)],'FontSize',10);
-text (.1, 12, ['Eye amp: ', num2str(eyevelH_amp,3)],'FontSize',10);
-legend({'t_3_0 - t_0 mean (Desaccaded full cycle removed)', 't_3_0 - t_0 mean (Desaccaded segments)','Sine fit','Stimulus'},'EdgeColor','w')
-drawnow;
-
-%% plot difference of raw traces
-figure(1067); clf;
-ttCycle = (1:cycleLength)/samplerate;
-plot(ttCycle, startEyeVelCycleMean,'m'); hold on
-plot(ttCycle, endEyeVelCycleMean, 'r'); hold on
-plot(ttCycle, BlueDiff,'b', 'LineWidth', 1);
-plot(ttCycle, headMean, 'k',ttCycle,zeros(size(ttCycle)),'--k'); hold on
-
-% Cosmetics of plot
-ylim([-15 15]);   xlim([0 max(ttCycle)])
-ylabel('deg/s');  xlabel('Time (s)');
-title(['Mean t_0, Mean t_3_0, & Delta Hor. Eye Vel: ', datatype(1:8) ' ']);
-legend({'t_0 mean (Desaccaded full cycle removed)', 't_3_0 mean (Desaccaded segments)','t_3_0 mean - t_0 mean (Desaccaded segments)','Stimulus','zero'},'EdgeColor','w')
-drawnow;
-
-%% calc Sine fit for startEyeVelCycleMean & endEyeVelCycleMean
-figure(1068); clf;
-[STARTeyevelH_offset, STARTeyevelH_rel_phase, STARTeyevelH_amp, STARTeyeHgain] = VORsineFitMaxMod( sinefreq(1), data(1).samplerate, startEyeVelCycleMean',mean(headVelSegments(1:3,:), 1)', mean(drumVelSegments(1:3,:), 1)');
-[ENDeyevelH_offset, ENDeyevelH_rel_phase, ENDeyevelH_amp, ENDeyeHgain]         = VORsineFitMaxMod( sinefreq(1), data(1).samplerate, endEyeVelCycleMean', mean(headVelSegments(4:end,:), 1)', mean(drumVelSegments(4:end,:), 1)');
-
-% plot differences in sine fits
-plot(ttCycle, STARTeyevelH_offset+sin(2*pi*freq*ttCycle + deg2rad(STARTeyevelH_rel_phase+180))*STARTeyevelH_amp,'m'); hold on
-plot(ttCycle, ENDeyevelH_offset+sin(2*pi*freq*ttCycle + deg2rad(ENDeyevelH_rel_phase+180))*ENDeyevelH_amp,'r'); hold on
-plot(ttCycle, eyevelH_offset+sin(2*pi*freq*ttCycle + deg2rad(eyevelH_rel_phase+180))*eyevelH_amp,'b')
-plot(ttCycle, headMean, 'k',ttCycle,zeros(size(ttCycle)),'--k'); hold on
-
-% Cosmetics of plot
-figure(1068)
-ylim([-15 15]);   xlim([0 max(ttCycle)])
-ylabel('deg/s');  xlabel('Time (s)');
-title(['Sine Fit Comparison: t_3_0, t_0 mean, & Difference  Hor. Eye Vel:', datatype(1:8) ' ']);
-legend({'t_0 mean Sine Fit (Desaccaded full cycle removed)', 't_3_0 mean Sine Fit (Desaccaded segments)','t_3_0 mean - t_0 mean Sine Fit (Desaccaded segments)', 'Stimulus','zero'},'EdgeColor','w')
-drawnow;
-
-%% add the new delta data to the results structure
-vec1 = NaN(length(R.header), 1)';
-vec1(4) =  eyeHgain;
-vec1(5) =  eyevelH_rel_phase;
-vec2 = NaN(length(R.header), 1)';
-vec2(4) =  ENDeyeHgain - STARTeyeHgain;
-vec2(5) = ENDeyevelH_rel_phase - STARTeyevelH_rel_phase;
-R.data(trueSegments+1,:) = vec1;
-R.data(trueSegments+2,:) = vec2;
-while length(R.labels) < trueSegments+2
-    R.labels = [R.labels; 'Apples'];
 end
