6845a43ea934c7be5aabd22c6d02b07343609381
max.gagnon11@gmail.com <max.gagnon11@gmail.com>
2018-12-13 16:01:49 -0800

    Update Sriram OKR Summary Analysis (WIP, do not use!!)

--- Tools/VOR_Analysis/Unique_Summaries/VOR_Summary_Sriram_OKR_Overview.m
+++ Tools/VOR_Analysis/Unique_Summaries/VOR_Summary_Sriram_OKR_Overview.m
@@ -5,43 +5,46 @@ clear;clc;
 try 
     load('diffData.mat')
 catch
-    warning('No Difference Data File Found!')
+    warning('No Difference diffData File Found!')
 end
 
 T = readtable('diffData.xlsx');
+color = T.color;
+conditions = unique(T.condition);
 
-conditionAmt = unique(T.condition);
-
-%% plot each condition
-for i = 1:length(conditionAmt)
-    currentCond = contains({diffData.condition}, conditionAmt{i});
-    subT = diffData(currentCond);
-    color = T(currentCond,:).color;
+%% Plot Each Condition
+for i = 1:length(conditions)
+    
     A = figure(i);clf
     
-    
-    for j = 1:length(subT)
+    % For each Mouse
+    for j = 1:length(diffData)
         
         % only plot the ones user wants
-        if subT(j).keep
+        if diffData(j).keep && contains(diffData(j).condition, conditions{i})
+            
+            % replace the color
+            diffData(j).color = color(j);
             
             subplot(2,1,1)
-            % TODO Color choice
-            plot(subT(j).ttCycle, subT(j).diffRaw, color{j}); hold on
+            plot(diffData(j).ttCycle, diffData(j).diffRaw, color{j}); hold on
             
-            [maxVal, maxLoc] = max(subT(j).diffRaw);
-            maxLoc = maxLoc/length(subT(j).diffRaw);
+            [maxVal, maxLoc] = max(diffData(j).diffRaw);
+            maxLoc = maxLoc/length(diffData(j).diffRaw);
             line([maxLoc maxLoc], [0 maxVal], 'color', color{j}, 'lineStyle', '--');
             scatter(maxLoc, maxVal, color{j}, 'filled')
             
             subplot(2,1,2)
-            % TODO Color choice
-            plot(subT(j).ttCycle, subT(j).diffFit, color{j}); hold on
+            plot(diffData(j).ttCycle, diffData(j).diffFit, color{j}); hold on
             
-            [maxVal, maxLoc] = max(subT(j).diffFit);
-            maxLoc = maxLoc/length(subT(j).diffFit);
+            [maxVal, maxLoc] = max(diffData(j).diffFit);
+            maxLoc = maxLoc/length(diffData(j).diffFit);
             line([maxLoc maxLoc], [0 maxVal], 'color', color{j}, 'lineStyle', '--');
             scatter(maxLoc, maxVal, color{j}, 'filled')
+            
+            
+            
+            
             
         end
     end
@@ -49,12 +52,12 @@ for i = 1:length(conditionAmt)
      subplot(2,1,1)
      hline(0, ':k')
      ylim([-30, 30])
-     title([subT(1).condition, ' Raw'])
+     title([diffData(1).condition, ' Raw'])
                               
      subplot(2,1,2)
      hline(0, ':k')
      ylim([-30, 30])
-     title([subT(1).condition, ' Fit'])
+     title([diffData(1).condition, ' Fit'])
      
      xlabel('Seconds')
     
