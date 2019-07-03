cf65cd1a9e16cf6409e9e51c647e32b55b1683b8
Max's Office PC <max.gagnon11@gmail.com>
2019-07-02 17:42:10 -0700

    Add initial scripts for Alex's smr+Bonsai Allignment & combining code

--- Tools/Program_MountainSort/activateCEDS64.m
+++ Tools/Program_MountainSort/activateCEDS64.m
@@ -1,10 +1,11 @@
-%% This script actiates the CEDS64 library aka Spike2 MATLAB SON Interface 
+function  activateCEDS64
+% This script actiates the CEDS64 library aka Spike2 MATLAB SON Interface 
 %
 % see http://ced.co.uk/upgrades/spike2matson for more information
+% Each computer MUST install this!
 % 
 % Maxwell Gagnon
 
-% add path to CED code
 if isempty(getenv('CEDS64ML'))
     setenv('CEDS64ML', 'C:\CEDMATLAB\CEDS64ML');
 end
@@ -12,3 +13,4 @@ cedpath = getenv('CEDS64ML');
 addpath(cedpath);
 
 CEDS64LoadLib( cedpath );
+
