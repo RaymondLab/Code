d4ca1390de1ad792d9ac2643b2c1e19183d1e18b
Max's Office PC <max.gagnon11@gmail.com>
2019-07-19 10:54:06 -0700

    Update Max's CF Project

--- Projects/addChanBrainstorm.m
+++ Projects/addChanBrainstorm.m
@@ -1,13 +1,53 @@
 %% Open File
-clear;clc;close all
+clear;clc;
 dbstop if error
-smrFile = 'C:\Users\maxwellg\Desktop\TempProjectData\Sriram\OKR\2018_09_24_2\2018_09_24_2.smr';
+smrFile = 'G:\My Drive\Expmt Data\2019_07 - Alex Bonsai+Spike2 Sync Test Files\07125\spike2generated07125.smr';
 
 %% Step 1 Allign Data
 
+% Read Bonsai csv Files
+DataTable = readtable('G:\My Drive\Expmt Data\2019_07 - Alex Bonsai+Spike2 Sync Test Files\07125\data07125.csv');
+testTable = readtable('G:\My Drive\Expmt Data\2019_07 - Alex Bonsai+Spike2 Sync Test Files\07125\test07125.csv');
 
+% Extract the Time Stamps
+aa = DataTable.Var1;
+bb = testTable.Var1(contains(testTable.Var2, 'True'));
+
+% Use only relative time
+bb = bb - aa(1);
+aa = aa - aa(1);
+
+% Extract TTL+ Pulses from smr file
+chanlist = readSpikeFile(smrFile,[]);
+chanindsAll = [chanlist.number];
+rawdata = importSpike(smrFile,chanindsAll(8));
+TTLpos = rawdata(1).data;
+
+% ms --> s
+bb = bb ./ 1000;
+aa = aa ./ 1000;
+
+%% debug plotting
+figure()
+ha = tight_subplot(2,1,[0 0],[.03 .03],[.03 .03]);
+axes(ha(1))
+vline([0; bb], 'k')
+xlim([0 100])
+axes(ha(2))
+vline([0; TTLpos], 'k')
+xlim([0 100])
+%linkaxes(ha, 'x')
+
+bbIntervals = bb(2:end) - bb(1:end-1);
+TTLposIntervals = TTLpos(2:end) - TTLpos(1:end-1);
+figure()
+scatter(TTLposIntervals, zeros(1,length(TTLposIntervals))); hold on
+scatter(bbIntervals, ones(1,length(bbIntervals)));
+xlim([.999 1.004])
+ylim([-.5 1.5])
 %% Step 2 SMR Modification
-% copy file
+
+% Copy File
 newSmrHandle = smrCopy(smrFile, '_FP');
 
 % Add fist channel
