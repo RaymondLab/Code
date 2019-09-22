900abc59914d33634ca68b54ec2d04c8e20184b5
Selvanar <max.gagnon11@gmail.com>
2019-09-22 16:37:23 -0700

    Update Calibration app. Not Functional

---
+++ EyeTracking/plotresults_APP.m
@@ -0,0 +1,36 @@
+function plotresults_APP(app, results)
+
+%% Plot raw data
+clf;
+alldata = [results.pupil1(:,1) results.cr1a(:,1:2)...
+           results.pupil2(:,1) results.cr2b(:,1:2)];
+ylims = [min(alldata(:)) max(alldata(:))];
+
+%% Camera 1
+hold(app.UIAxes3, 'on')
+hp(3) = plot(app.UIAxes3, results.time1, results.cr1a(:,2),'b-'); hold on;
+hp(1) = plot(app.UIAxes3, results.time1, results.pupil1(:,1),'r-'); hold on
+hp(2) = plot(app.UIAxes3, results.time1, results.cr1a(:,1),'k-');
+
+xlim(app.UIAxes3, [results.time2(1+1) results.time2(end-1)]); box off
+ylim(app.UIAxes3, ylims)
+ylabel(app.UIAxes3, 'Cam1 pos (pix)')
+hold(app.UIAxes3, 'off')
+
+
+%% Camera 2
+hold(app.UIAxes3_2, 'on')
+
+plot(app.UIAxes3_2, results.time2,results.cr2b(:,2),'b-'); hold on;
+plot(app.UIAxes3_2, results.time2, results.pupil2(:,1),'r-'); hold on;
+plot(app.UIAxes3_2, results.time2,results.cr2b(:,1),'k-');
+
+xlim(app.UIAxes3_2, [results.time2(1) results.time2(end)]); box off
+ylim(app.UIAxes3_2, ylims)
+xlabel(app.UIAxes3_2, 'Time (s)')
+ylabel(app.UIAxes3_2, 'Cam2 pos (pix)')
+hold(app.UIAxes3_2, 'off')
+
+
+%legend(app.UIAxes3_2, hp,{'Pupil - X','Corneal Reflection - X','Corneal Reflection - Y'})
+
