a5641717abbc15cb8b67b51a81aaa32397e75cd4
Max's Office PC <max.gagnon11@gmail.com>
2019-10-16 19:06:06 -0700

    update CF project

--- Projects/Max - CF Project/getMetadata.m
+++ Projects/Max - CF Project/getMetadata.m
@@ -2,146 +2,177 @@
 clear;clc; close all
 A = chanData;
 
-% open Excel File
-A.excelFile = 'C:\Users\Public\RaymondLabCode\Projects\Max - CF Project\Monkey Metadata by Max.xlsx';
-%A.excelFile = 'C:\Users\Public\RaymondLabCode\Projects\Max - CF Project\Monkey Metadata by Max (Akira).xlsx';
-A.expmt_table = readtable(A.excelFile);
-
-A.expmtDataFolder = 'D:\My Drive\Expmt Data\Max\Climbing Fiber Project\Jennifer Data\jennifer_arch';
-%A.expmtDataFolder = 'G:\My Drive\Expmt Data\2019_05 - Akira and Sriram Complex Spikes';
+A.expmtDataFolder = 'G:\My Drive\Expmt Data\Max\Climbing Fiber Project\Jennifer Data\jennifer_arch';
 
 A.bFiles = dir([A.expmtDataFolder '\**\*']);
-A.bFiles(~contains({A.bFiles.name}, {'behavior'})) = [];
+eFiles = A.bFiles(contains({A.bFiles.name}, {'ephys.mat'}));
 
-%A.whoseData = 'Akira';
-A.whoseData = 'Jennifer';
-
-
-%% Load data instead
-clear;clc;close all
-load C:\Users\maxga\Desktop\RecList.mat
-load('C:\Users\Public\RaymondLabCode\Projects\Max - CF Project\shiftVals')
-plotOn = 1;
-
-%A.bFiles(~contains({A.bFiles.name}, {'behavior'})) = [];
 A.bFiles([A.bFiles.isdir]) = [];
-%A.bFiles(~contains({A.bFiles.name}, {'.mat'})) = [];
-%A.bFiles(contains({A.bFiles.name}, {'da0301'})) = [];
-%A.bFiles(contains({A.bFiles.name}, {'da02'})) = [];
-A.bFiles(~contains({A.bFiles.name}, {'aligned'})) = [];
-
+A.bFiles(~contains({A.bFiles.name}, {'.'})) = [];
+A.bFiles(contains({A.bFiles.name}, {'.dat'})) = [];
+A.bFiles(contains({A.bFiles.name}, {'.mat'})) = [];
+A.bFiles(contains({A.bFiles.name}, {'.ini'})) = [];
+A.bFiles(contains({A.bFiles.name}, {'du'})) = [];
+A.bFiles(contains({A.bFiles.name}, {'eu'})) = [];
 files = A.bFiles;
 
-%% Initial Loop to Align Files
-% for j = 1:length(A.bFiles)
-%     
-%     % Skip Ephys files
-%     if ~contains({A.bFiles(j).name}, {'behavior.mat'})
-%         continue
-%     end
-%     
-%     if exist(fullfile(A.bFiles(j).folder, strrep(A.bFiles(j).name, 'behavior', 'aligned')), 'file')
-%         continue
-%     end
-%     
-% 
-%     % Keep track of where you are
-%     disp([A.bFiles(j).name, '(', num2str(j), '/', num2str(length(A.bFiles)), ')'])
-%     
-%     % Open Behavior
-%     filePath = fullfile(A.bFiles(j).folder, A.bFiles(j).name);
-%     load(filePath)
-%     if length(datchandata(behaviorData,'ss')) < 200
-%         disp('     No ss Timestamps')
-%         continue
-%     end
-%     
-%     % Open Ephys
-%     ephysName = strrep(A.bFiles(j).name, 'behavior', 'ephys');
-%     ephysEntry = A.bFiles(contains({A.bFiles.name}, ephysName));
-%     
-%     if ~isempty(ephysEntry)
-%         ephysPath = fullfile(ephysEntry.folder, ephysEntry.name);
-%         try
-%             load(ephysPath)
-%         catch
-%             disp('Ephys Error')
-%             continue
-%         end
-%         
-%         % Align Traces
-%         tic
-%         [behaviorEphysAligned, shiftAmt, shiftConfidence] = alignCXandMaestro(behaviorData, ephysData, plotOn);
-%         toc
-%         disp(shiftAmt)
-%         newName = strrep(ephysName, 'ephys', 'aligned');
-%         
-%         if shiftConfidence ~= 0
-%             save(fullfile(A.bFiles(j).folder, newName), 'behaviorEphysAligned');
-%         else
-%             disp('Alignment Failure')
-%         end
-% 
-%     else
-%         disp('     No Ephys File')
-%     end
-%     
-%     % Plot Traces
-%     
-% end
+%% Create Table
+name        = cell(length(files),1);
+behaviorMat = cell(length(files),1);
+ephysMat    = cell(length(files),1);
+alignedMat  = cell(length(files),1);
+sineStep    = cell(length(files),1);
 
+alignVal    = nan(length(files),1);
+freq        = nan(length(files),1);
+sineConfidence = nan(length(files),1);
 
 %% Double Check Loop
 
-%shiftVal = nan(length(A.bFiles),1);
-eSamplerate = 50000;%ephysDat.samplerate;
-%ephysData = datchandata(ephysDat,'Ephys');
- 
-
 for j = 1:length(files)
+    tic
+    disp(j)
     
-    if shiftVal(j) > 0 && shiftVal(j) < .1
+    
+    %% name
+    name{j} = files(j).name;
+    disp(name{j})
+    
+    
+    %% behaviorMat
+    behaviorMatName = [strrep(files(j).name, '.', '_'), 'behavior.mat'];
+    behExits = 0;
+    if exist(fullfile(files(j).folder, behaviorMatName), 'file')
+        behaviorMat{j} = behaviorMatName;
+        behExists = 1;
+    else
+        toc
         continue
     end
     
-    % Keep track of where you are
-    disp([files(j).name, '(', num2str(j), '/', num2str(length(files)), ')'])
-    disp(shiftVal(j));
-    
-    % Open Behavior
-    filePath = fullfile(files(j).folder, files(j).name);
-    load(filePath)
-    
-    timeEphys = 0:1/eSamplerate:(length(behaviorEphysAligned(1, 10).data)-1)/eSamplerate;
-    
-    
-    if shiftVal(j) < 0 || shiftVal(j) > .1
-        figure(2);clf
-        plot(timeEphys + behaviorEphysAligned(1, 10).tstart, ...
-             behaviorEphysAligned(1, 10).data);hold on
-        xlim([behaviorEphysAligned(1, 10).tstart, behaviorEphysAligned(1, 10).tstart+.5])
-        
-        vline(behaviorEphysAligned(1, 8).data(1:100)+behaviorEphysAligned(1, 8).tstart, 'r');
-        
-        [behaviorEphysAligned, shiftAmt, shiftConfidence] = alignCXandMaestro(behaviorEphysAligned, behaviorEphysAligned(1, 10).data, 1);
-        
-        disp(shiftConfidence)
-        if shiftConfidence > 150
-            temp = behaviorEphysAligned(end);
-            behaviorEphysAligned(10:end) = [];
-            behaviorEphysAligned = [behaviorEphysAligned, temp];
-            disp(shiftAmt)
-            save(fullfile(files(j).folder, files(j).name), 'behaviorEphysAligned');
-        end
-        
-        disp('apple')
+
+    %% ephysMat
+    ephysMatName = [strrep(files(j).name, '.', '_'), 'ephys.mat'];
+    ephysFile = eFiles(contains({eFiles.name}, {ephysMatName}));
+    if ~isempty(ephysFile)
+        ephysMat{j} = ephysMatName;
+    end
+
+
+    %% alignedMat
+    alignedMatName = [strrep(name{j}, '.', '_'), 'aligned.mat'];
+    alignValExists = 0;
+    if exist(fullfile(files(j).folder, alignedMatName), 'file')
+        alignedMat{j} = alignedMatName;
+        alignValExists = 1;
     end
     
-%     try
-%         shiftVal(j) = behaviorEphysAligned(1,10).tstart;
-%     catch
-%     end
+    
+    %% AlignVal
+    if alignValExists
+        try
+            load(fullfile(files(j).folder, alignedMat{j}))
+            alignVal(j) = behaviorEphysAligned(1,10).tstart;
+        catch
+        end
+    end
+
+
+    %% Stim Type & Freq
+    if behExists
+        try
+            load(fullfile(files(j).folder, behaviorMat{j}))
+
+            % just an estimation
+            freq(j) = findExpmtFreq(behaviorData);
+
+            % need freq estimation for finding sine/step
+            [sineStep{j}, sineConfidence(j)] = findSineStep(behaviorData, freq(j));
+
+            % overwrite freq if not sine
+            if ~contains(sineStep{j}, 'sine')
+                freq(j) = nan;
+            end
+
+        catch
+        end
+    end
+    
+    %%
+    toc
+
+end
+
+
+%% Combine, Create and Save the Table
+T = table(name, behaviorMat, ephysMat, alignedMat, alignVal, sineStep, sineConfidence, freq);
+writetable(T, 'G:\My Drive\Expmt Data\Max\Climbing Fiber Project\ExperimentMetadata.xlsx')
+
+
+%% Helpful functions
+function peakFreqEstimate = findExpmtFreq(datObj)
+
+    maxFreqLoc = [0,0,0,0,0; 0,0,0,0,0];
+    
+    for i = 1:7
+        if isempty(datObj(i).data)
+            continue
+        end
+
+        L = length(datObj(i).data);
+        Y = fft(datObj(i).data);
+        P2 = abs(Y/L);
+        P1 = P2(1:floor(L/2+1));
+        P1(2:end-1) = 2*P1(2:end-1);
+        f = datObj(i).samplerate*(0:(L/2))/L;
+
+        if sum(P1 == max(P1)) == 1
+            maxFreqLoc(1, i) = f(P1 == max(P1));
+            maxFreqLoc(2, i) = max(P1);
+        end
+    end
+    
+    peakFreqEstimate = maxFreqLoc(1,maxFreqLoc(2,:) == max(maxFreqLoc(2,:)));
+    peakFreqEstimate = round(peakFreqEstimate * 2)/2;
     
 end
 
-save('C:\Users\maxga\Desktop\shiftVals', 'shiftVal')
+function [stimType, maxr2] = findSineStep(datObj,freq)
+    warning off
+    r2 = nan(2,1);
+    
+    for i = [4, 5]
+        if isempty(datObj(i).data)
+            continue
+        end
+        timeVec = dattime(datObj(i));
+        y1 = sin(2*pi*freq*timeVec);
+        y2 = cos(2*pi*freq*timeVec);
+        const = ones(size(y1));
+        vars = [y1 y2 const];
+        [~,~,~,~,stat] = regress(datObj(i).data, vars);
+        %Amp = sqrt(b(1)^2+b(2)^2);
+        r2(i) = stat(1);
+    end
+    maxr2 = max(r2);
+    
+    if maxr2 > .1
+        stimType = 'sine';
+    elseif maxr2 == 0
+        stimType = [];
+    else
+        stimType = 'step';
+    end
+    
+    %disp(maxr2)
+    %disp(stimType)
+     
+    %figure(43); clf
+    %plot(datObj(4).data); hold on
+    %plot(datObj(5).data)
+    
+    if maxr2 > .1 && maxr2 < .7
+        %disp(max(r2))
+    end
+
+end
